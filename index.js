b991ef42edf26ec77c56d05cc226d4dd9a324dc7
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-06-12 11:45:02 +0530

    second coomit

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const client = new Discord.Client({
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+client.on("message", msg => {
+    if (msg.content === "hi") {
+        msg.reply("hello");
+    }
+})
+
+
 
 
 client.login(process.env.BOT_TOKEN);
